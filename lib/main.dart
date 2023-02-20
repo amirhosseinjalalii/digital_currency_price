6d8cdd4beb7f8b94178ec6005d3c1cd48210ae5f
amirhossein jalali <rimaj@amirhosseins-MacBook-Air.local>
2023-02-20 16:02:09 +0330

    init currency project

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  );
+  runApp(const MyMaterialApp());
+}
+
+class MyMaterialApp extends StatefulWidget {
+  const MyMaterialApp({super.key});
+
+  @override
+  State<MyMaterialApp> createState() => _MyMaterialAppState();
+}
+
+class _MyMaterialAppState extends State<MyMaterialApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Container(child: const Text("digital currency app")),
+        ),
+      ),
+    );
+  }
+}
