69edd2799e2e673f6da7aca5825f00026e00ed90
amirhossein jalali <rimaj@amirhosseins-MacBook-Air.local>
2023-02-22 11:09:20 +0330

    create moved text elevated button and choice chip

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,12 +1,27 @@
 import 'package:digital_currency_price/ui/ui_hellper/home_page_view.dart';
 import 'package:digital_currency_price/ui/ui_hellper/theme_switcher.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:marquee/marquee.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final PageController _pageViewController = PageController(initialPage: 0);
+
+  var defaultChoiceIndex = 0;
+
+  final List<String> _choiceList = [
+    "Top MarketCaps",
+    "Top Gainers",
+    "Top Losers"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +43,7 @@ class HomeScreen extends StatelessWidget {
           scrollDirection: Axis.vertical,
           child: Column(
             children: [
+              //page view
               Padding(
                 padding: const EdgeInsets.only(top: 10, left: 5, right: 5),
                 child: SizedBox(
@@ -59,6 +75,89 @@ class HomeScreen extends StatelessWidget {
                       )
                     ],
                   ),
+                ),
+              ),
+              //moving text
+              SizedBox(
+                width: double.infinity,
+                height: 30,
+                child: Marquee(
+                  text: '🔊 this is place for news in application ',
+                  style: textTheme.bodySmall,
+                ),
+              ),
+
+              const SizedBox(height: 5),
+              //elevated button
+              Padding(
+                padding: const EdgeInsets.only(left: 5, right: 5),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          backgroundColor: Colors.green[700],
+                          textStyle: textTheme.bodySmall,
+                        ),
+                        child: const Text("buy"),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          backgroundColor: Colors.red[700],
+                          textStyle: textTheme.bodySmall,
+                        ),
+                        child: const Text("sell"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(right: 5, left: 5),
+                child: Row(
+                  children: [
+                    Wrap(
+                      spacing: 8,
+                      children: List.generate(
+                        _choiceList.length,
+                        (index) => ChoiceChip(
+                          onSelected: (value) {
+                            setState(() {
+                              defaultChoiceIndex =
+                                  value ? index : defaultChoiceIndex;
+                            });
+                          },
+                          selectedColor: primaryColor,
+                          label: Text(
+                            _choiceList[index],
+                            style: GoogleFonts.ubuntu(
+                              color: defaultChoiceIndex == index
+                                  ? Colors.white
+                                  : Colors.black,
+                              fontSize: 12,
+                            ),
+                          ),
+                          selected: defaultChoiceIndex == index,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               )
             ],
